bcdcbf53d68d097ea60d32b2941d4f5c4ad864cb
vz-centsmart <vz-centsmart@users.noreply.github.com>
2025-07-07 00:59:24 +0800

    feat: editor frontend for hash game

--- bpk.js
+++ bpk.js
@@ -5,6 +5,9 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+    if (!mybutton) {
+        return;
+    }
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
     } else {
